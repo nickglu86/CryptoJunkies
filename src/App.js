cd99048060dfadf1631878347004fbe4f8dd1460
Nick Glushchenko <enayfls@gmail.com>
2021-08-30 18:15:21 +0300

    added basename to App.js Route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-      <Router >
+      <Router basename="/crypto-site">
         <main>
             <Header />
             <Switch>
