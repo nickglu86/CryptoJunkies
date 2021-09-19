fa9a974a1b56195f63a26995f4b466b66d574b1c
Nick Glushchenko <enayfls@gmail.com>
2021-09-19 23:55:07 +0300

    Invest Page

--- src/pages/Invest.js
+++ src/pages/Invest.js
@@ -1,6 +1,31 @@
 const Invest = () => {
     return ( 
-         <div className="page-header"> Invest Page </div> 
+        <section className="invest">
+            <div className="invest-container">
+            <a href="https://accounts.binance.com/en/register?ref=10799690">
+                <div className="binance-wrapper">
+                      <div className="binance"></div>
+                      <div className="binance-content">
+                         <h2>Binance</h2>
+                        <p>Binance is a cryptocurrency exchange which is currently the largest exchange in the world in terms of daily trading volume of cryptocurrencies. It was founded in 2017 and is registered in the Cayman Islands. Binance was founded by Changpeng Zhao, a developer who had previously created high frequency trading software.</p>
+                        <div className="sign-up">SignUp Now</div>
+                      </div>
+                    
+                </div>
+            </a>
+             <a href="https://cex.io/r/0/up106177983/0/">
+                <div className="cexio-wrapper">
+                    <div className="cexio"></div>
+                    <div className="cexio-content">
+                         <h2>CEX.io</h2>
+                      <p>CEX.IO was one of the first platforms to make fiat-to-crypto transactions accessible by offering card payments and bank transfers to the clients. The leading cryptocurrency exchange  offers Bitcoin, ETH and other trading options , provides 24/7 customer support, high level of security, and stable deposits and withdrawals.</p> 
+                      <div className="sign-up">SignUp Now</div>
+                    </div>
+                </div> 
+             </a>
+            </div>
+        </section>
+       
           );
 }
  
